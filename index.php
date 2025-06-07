13feae64e66768f9ec271b30766b42f18a67579c
dima.moisieienko <dimonmail09@gmail.com>
2025-06-07 11:52:12 +0300

    separated logic and view in different files

--- index.php
+++ index.php
@@ -1,85 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Demo</title>
-</head>
-
-<body>
-
 <?php
-include_once './functions.php';
 
 $books = [
     [
         'name' => 'Book 1',
         'author' => 'Author 1',
-        'purchaseUrl' => 'https://unian.net',
+        'purchaseUrl' => 'https://unian2.net',
+        'releaseYear' => 2002,
     ],
     [
         'name' => 'Book 2',
         'author' => 'Author 1',
         'purchaseUrl' => 'https://unian.net',
+        'releaseYear' => 2002,
     ],
     [
         'name' => 'Book 3',
         'author' => 'Author 3',
         'purchaseUrl' => 'https://unian.net',
+        'releaseYear' => 2003,
     ],
     [
         'name' => 'Book 1',
         'author' => 'Author 3',
-        'purchaseUrl' => 'https://unian.net',
+        'purchaseUrl' => 'https://unian2.net',
+        'releaseYear' => 1999,
     ],
     [
         'name' => 'Book 2',
         'author' => 'Author 4',
         'purchaseUrl' => 'https://unian.net',
+        'releaseYear' => 1988,
     ],
     [
         'name' => 'Book 3',
         'author' => 'Author 5',
         'purchaseUrl' => 'https://unian.net',
+        'releaseYear' => 2002,
     ],
     [
         'name' => 'Book 1',
         'author' => 'Author 6',
         'purchaseUrl' => 'https://unian.net',
+        'releaseYear' => 2009,
     ],
     [
         'name' => 'Book 2',
         'author' => 'Author 7',
         'purchaseUrl' => 'https://unian.net',
+        'releaseYear' => 2008,
     ],
     [
         'name' => 'Book 3',
         'author' => 'Author 8',
         'purchaseUrl' => 'https://unian.net',
+        'releaseYear' => 2000,
     ],
 ];
 
-?>
 
-<h1>Recomended books:</h1>
-<ul>
-  <?php
+$filteredBooks = array_filter($books, function($book){
+    return $book['name']==='Book 1';
+  });
 
-  $filteredBooks = filterByAuthor($books, 'Author 3');
-// var_dump($filteredBooks);
+  // var_dump($filteredBooks);
 // die();
 
-foreach ($filteredBooks as $book) { ?>
-    <li>
-    <a href="<?php echo $book['purchaseUrl']; ?>"><?php echo $book['author']; ?></a>
-      <?php echo $book['name']; ?>
-    </li>
-  <?php } ?>
- 
-</ul>
-
-
-</body>
-
-</html>
+require "index.view.php";
---
+++ index.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Demo</title>
+</head>
+
+<body>
+<h1>Recomended books:</h1>
+<ul>
+  <?php
+foreach ($filteredBooks as $book) { ?>
+    <li>
+    <a href="<?php echo $book['purchaseUrl']; ?>"><?php echo $book['author']; ?></a>
+      <?php echo $book['name']; ?>
+    </li>
+  <?php } ?>
+ 
+</ul>
+
+
+</body>
+
+</html>
