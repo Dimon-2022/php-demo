7a8e5d28bc8d9d89f615089b81919ecb1a214cc5
dima.moisieienko <dimonmail09@gmail.com>
2025-06-15 14:01:39 +0300

    created Validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,21 +1,19 @@
 <?php
 
+require 'Validator.php';
+
 $heading = 'Create Note';
 
 $config = require 'config.php';
 $db = new Database($config['database']);
 
-$body = isset($_POST['body']) ? $_POST['body'] : '';
+$body = $_POST['body'] ?? '';
 
 if ('POST' === $_SERVER['REQUEST_METHOD']) {
     $errors = [];
 
-    if (0 === strlen($_POST['body'])) {
-        $errors['body'] = 'A body is required';
-    }
-
-    if (strlen($_POST['body']) >= 15) {
-        $errors['body'] = 'Too many symbols';
+    if (!Validator::string($body, 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1000 characters is required';
     }
 
     if (empty($errors)) {
