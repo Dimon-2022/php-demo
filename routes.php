834518bbe8e332e2c57d5da38d67006d745d843f
dima.moisieienko <dimonmail09@gmail.com>
2025-06-21 16:00:49 +0300

    added comments for router

--- routes.php
+++ routes.php
@@ -4,15 +4,15 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php');//all notes
 $router->get('/note', 'controllers/notes/show.php');
-$router->delete('/note', 'controllers/notes/destroy.php');
+$router->delete('/note', 'controllers/notes/destroy.php');//delete note 
 
 
-$router->get('/notes/create', 'controllers/notes/create.php');
-$router->post('/notes', 'controllers/notes/store.php');
+$router->get('/notes/create', 'controllers/notes/create.php');//show form for creating note
+$router->post('/notes', 'controllers/notes/store.php');//store note after hitting submit button on create form 
 
 
-$router->get('/note/edit', 'controllers/notes/edit.php');
+$router->get('/note/edit', 'controllers/notes/edit.php');//show edit form of note 
 
-$router->patch('/note', 'controllers/notes/update.php');
+$router->patch('/note', 'controllers/notes/update.php');//update note after hiiting submit button on edit form
