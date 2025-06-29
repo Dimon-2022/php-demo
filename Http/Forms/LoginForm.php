0eac79e9e328bd2e3c6e964e2fc0ed590a2b38bd
dima.moisieienko <dimonmail09@gmail.com>
2025-06-29 14:09:32 +0300

    refactor validation form

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -2,30 +2,50 @@
 
 namespace Http\Forms;
 
+use Core\ValidationException;
 use Core\Validator;
 
 class LoginForm
 {
     protected $errors = [];
+    public $attributes;
 
-    public function validate($email, $password)
+    public function __construct($attributes)
     {
-        if (!Validator::email($email)) {
+        $this->attributes = $attributes;
+        if (!Validator::email($attributes['email'])) {
             $this->errors['email'] = 'Please provide a valid email adress';
         }
 
-        if (!Validator::string($password)) {
+        if (!Validator::string($attributes['password'])) {
             $this->errors['password'] = 'Please provide a valid password.';
         }
-
-        return empty($this->errors);
     }
 
-    public function errors(){
+    public static function validate($attributes)
+    {
+        $instance = new static($attributes);
+
+        if ($instance->failed()) {
+            // throw new ValidationException();
+            ValidationException::throw($instance->errors(), $instance->attributes);
+        }
+
+        return $instance;
+    }
+
+    public function failed()
+    {
+        return (bool) count($this->errors);
+    }
+
+    public function errors()
+    {
         return $this->errors;
     }
 
-    public function error($field,$message){
+    public function error($field, $message)
+    {
         $this->errors[$field] = $message;
     }
 }
--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -2,29 +2,22 @@
 
 use Core\Authenticator;
 use Core\Session;
+use Core\ValidationException;
 use Http\Forms\LoginForm;
 
-// receive credentials
-$email = $_POST['email'] ?? '';
-$password = $_POST['password'] ?? '';
+try {
+    $form = LoginForm::validate($attributes = [
+        'email' => $_POST['email'],
+        'password' => $_POST['password'],
+    ]);
+} catch (ValidationException $exception) {
+    Session::flash('errors', $exception->errors);
+    Session::flash('old', $exception->old);
 
-$form = new LoginForm();
-
-if ($form->validate($email, $password)) {
-    $auth = new Authenticator();
-
-    if ($auth->attempt($email, $password)) {
-        redirect('/');
-    } else {
-        $form->error('email', 'No matching account found for that email adress and password');
-    }
+    return redirect('/login');
 }
 
-Session::flash('errors', $form->errors());
-Session::flash('old',
-    [
-        'email' => $email,
-    ]
-);
-
-return redirect('/login');
+if ((new Authenticator())->attempt($attributes['email'], $attributes['password'])) {
+    redirect('/');
+}
+$form->error('email', 'No matching account found for that email adress and password');
